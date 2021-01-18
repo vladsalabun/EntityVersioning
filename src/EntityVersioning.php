404e10cb746ee1d8423d560bb5653037277d0a93
Vlad <salabunv@ukr.net>
2021-01-18 16:06:53 +0200

    Initial commit

---
+++ src/EntityVersioning.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  This package takes control with entity versions.
+ */
+namespace Salabun;
+
+class EntityVersioning
+{
+    public static function compare()
+    {
+        return 'EntityVersioning: Hello World!';
+    }
+}
