51006c7c612f259d556629f80caaf118655c7bcc
Vlad <salabunv@ukr.net>
2021-01-25 03:25:07 +0200

    Update StoredData.php

--- src/EntityHelpers/StoredData.php
+++ src/EntityHelpers/StoredData.php
@@ -20,41 +20,41 @@ Trait StoredData
     /** 
      *  Додати збережені властивості:
      */
-	public function addStoredProperties($array) 
-	{
+    public function addStoredProperties($array) 
+    {
         if(is_array($array)) {
             $this->storedProperties = array_merge($this->storedProperties, $array);
         }
         
         return $this;
-	}
+    }
 
     /** 
      *  Додати версії збережених властивостей:
      */
-	public function addStoredPropertiesVersions($array) 
-	{
+    public function addStoredPropertiesVersions($array) 
+    {
         if(is_array($array)) {
             $this->storedPropertiesVersions = array_merge($this->storedPropertiesVersions, $array);
         }
         
         return $this;
-	}
+    }
     
     /** 
      *  Повертає збережені властивості:
      */
-	public function getStoredProperties() 
-	{
+    public function getStoredProperties() 
+    {
         return $this->storedProperties;
-	}
+    }
     
     /** 
      *  Повертає версії збережених властивостей:
      */
-	public function getStoredPropertiesVersions()
-	{
+    public function getStoredPropertiesVersions()
+    {
         return $this->storedPropertiesVersions;
-	}
+    }
 
 }
