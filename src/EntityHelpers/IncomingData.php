e808ecc2000eb9acb27b5af865feaa8add2c3983
Vlad <salabunv@ukr.net>
2021-01-25 03:24:13 +0200

    Update IncomingData.php

--- src/EntityHelpers/IncomingData.php
+++ src/EntityHelpers/IncomingData.php
@@ -20,41 +20,41 @@ Trait IncomingData
     /** 
      *  Додати вхідні властивості:
      */
-	public function addIncomingProperties($array) 
-	{
+    public function addIncomingProperties($array) 
+    {
         if(is_array($array)) {
             $this->incomingProperties = array_merge($this->incomingProperties, $array);
         }
         
         return $this;
-	}
+    }
 
     /** 
      *  Додати версії вхідних властивостей:
      */
 	public function addIncomingPropertiesVersions($array) 
-	{
+    {
         if(is_array($array)) {
             $this->incomingPropertiesVersions = array_merge($this->incomingPropertiesVersions, $array);
         }
         
         return $this;
-	}
+    }
     
     /** 
      *  Повертає вхідні властивості:
      */
-	public function getIncomingProperties() 
-	{
+    public function getIncomingProperties() 
+    {
         return $this->incomingProperties;
-	}
+    }
     
     /** 
      *  Повертає версії вхідних властивостей:
      */
-	public function getIncomingPropertiesVersions() 
-	{
+    public function getIncomingPropertiesVersions() 
+    {
         return $this->incomingPropertiesVersions;
-	}
+    }
     
 }
